168db1fd6fdb35095f4a4f489c9424a92e8fceb1
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-08-30 15:57:08 +0200

    test: Existence of columns GREEN

--- src/connectfour.js
+++ src/connectfour.js
@@ -1,4 +1,5 @@
 const { Player} = require('../src/player.js');
+const { Rack} = require('../src/rack.js');
 class ConnectFour {
     players = []
     rack = ''
@@ -6,6 +7,8 @@ class ConnectFour {
     constructor() {
         this.players.push(new Player('YELLOW'))
         this.players.push(new Player('RED'))
+        this.rack = new Rack();
+        this.rack.columns = [];
     }
 }
 
---
+++ src/rack.js
@@ -0,0 +1,5 @@
+class Rack {
+
+}
+
+module.exports = {Rack}
