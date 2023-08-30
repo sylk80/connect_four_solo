6d7bde28ed0907dd228a2e5c35db161b6c0da701
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-08-30 14:54:30 +0200

    chore: Refactor

--- __tests__/connectfour.test.js
+++ __tests__/connectfour.test.js
@@ -1,18 +1,15 @@
 const { ConnectFour} = require('../src/connectfour.js');
 
 describe('Connect four testing, there...', () => {
+    const game = new ConnectFour();
+    const players = game.players;
     test('should be a ConnectFour class...', () => {
-      const game = new ConnectFour();
       expect(game).toBeDefined();
     });
     test('should be a ConnectFour class with players...', () => {
-        const game = new ConnectFour();
-        const players = game.players;
         expect(players).toBeDefined();
     });
     test('should be two players...', () => {
-        const game = new ConnectFour();
-        const players = game.players;
         expect(players).toHaveLength(2);
     });
   });
--- src/connectfour.js
+++ src/connectfour.js
@@ -2,6 +2,5 @@ class ConnectFour {
     players = ['RED','YELLOW']
 }
 
-const template_method = () => "Hello world";
 
-module.exports = {ConnectFour, template_method}
+module.exports = {ConnectFour}
