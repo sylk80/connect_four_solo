1eecdefe4dc8ed5aa3a4d9f0efd61983169693a9
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-09-20 15:53:01 +0200

    test: return lowest position RED

--- __tests__/rack.test.js
+++ __tests__/rack.test.js
@@ -6,6 +6,7 @@ describe('Rack testing, there...', () => {
     const tokenRed = new Token('RED')
     rack.place(tokenRed)
     const tokens = rack.tokens;
+    const firstPlaced = tokens[0]
     test('should be a Rack...', () => {
         expect(rack).toBeDefined();
     });
@@ -19,15 +20,17 @@ describe('Rack testing, there...', () => {
         expect(tokens[0]).toBeDefined()
     });
     test('token object should be found in the rack in it\'s position...', () => {
-        const firstPlaced = tokens[0]
         expect(firstPlaced.column).toEqual(tokenRed.column)
         expect(firstPlaced.row).toEqual(tokenRed.row)
     });
     test('token object should be found in the lowest position...', () => {
-        const firstPlaced = tokens[0]
         expect(firstPlaced.row).toEqual(rack.rows[0])
     });
     test('should search for lowest position...', () => {
         expect(rack.lowestPosition).toBeDefined()
+    });
+    test('should return lowest position...', () => {
+        const nextLowest = rack.lowestPosition(firstPlaced.column)
+        expect(nextLowest).not.toEqual(firstPlaced.row)
     });
 })
