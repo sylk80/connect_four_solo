444dc135a5f063becc2d6e8c727e10d23082f883
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-09-20 14:43:51 +0200

    test: token object placing in rack RED

--- __tests__/rack.test.js
+++ __tests__/rack.test.js
@@ -1,11 +1,17 @@
 const { Rack} = require('../src/rack');
+const {Token} = require("../src/token");
 
 describe('Rack testing, there...', () => {
     const rack = new Rack()
+    const tokenRed = new Token('RED')
     test('should be a Rack...', () => {
         expect(rack).toBeDefined();
     });
     test('tokens are placed in the rack...', () => {
         expect(rack.place).toBeDefined();
+    });
+    test('token object is placed in the rack...', () => {
+        rack.place(tokenRed)
+        expect(rack.tokens).toBeDefined();
     });
 })
