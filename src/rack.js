63d9a614b9ac1a9b7e62215e7a8abebfb6a597a4
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-09-20 15:02:14 +0200

    test: token object found in rack in the lowest position GREEN

--- src/rack.js
+++ src/rack.js
@@ -1,10 +1,12 @@
 class Rack {
     columns = new Array(7)
-    rows = new Array(6)
+    rows = [1,2,3,4,5,6]
     tokens = []
 
     place(token) {
+        token.row = this.rows[0]
         this.tokens.push(token)
+
     }
 }
 
